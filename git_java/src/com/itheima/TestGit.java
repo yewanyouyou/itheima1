b2bc03bf4d12a30a71346f2d005ffe4a2660bba8
libai <zhhkam@163.com>
2019-07-10 20:10:04 +0800

    测试ide

--- git_java/src/com/itheima/TestGit.java
+++ git_java/src/com/itheima/TestGit.java
@@ -3,5 +3,8 @@ package com.itheima;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println(112131);
+        System.out.println(112131);
+        System.out.println(112131);
+        System.out.println(112131);
     }
 }

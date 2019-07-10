e2ae3e428cc591b690321ed143afd5714c8a306f
libai <zhhkam@163.com>
2019-07-10 21:34:34 +0800

    分支提交a

--- git_java/src/com/itheima/TestGit2.java
+++ git_java/src/com/itheima/TestGit2.java
@@ -4,6 +4,7 @@ public class TestGit2 {
     public static void main(String[] args) {
         System.out.println("22222222");
         System.out.println("fezhidev");
-        System.out.println("分支提交");
+        System.out.println("主线提交");
+        System.out.println("主线提交");
     }
 }

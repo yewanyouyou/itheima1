fc40ecf15ddf154869a92ed3476a833608116298
libai <zhhkam@163.com>
2019-07-12 19:05:10 +0800

    zhuxian2

--- git_java/src/com/itheima/TestDev.java
+++ git_java/src/com/itheima/TestDev.java
@@ -6,5 +6,6 @@ public class TestDev {
         System.out.println("主线提交1");
         System.out.println("分支提交2");
         System.out.println("分支提交3");
+        System.out.println("主线提交2");
     }
 }
